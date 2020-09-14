f428c7864835a4398c8dff5783643be822bed47c
Gael Monachon <gael.monachon@rexel.fr>
2020-09-14 13:44:08 +0200

    revert

--- AngularHybridTest/src/Application.js
+++
@@ -1,6 +0,0 @@
-var AngularHybridTest;
-(function (AngularHybridTest) {
-    'use strict';
-    var angularHybrid;
-})(AngularHybridTest || (AngularHybridTest = {}));
-//# sourceMappingURL=Application.js.map
