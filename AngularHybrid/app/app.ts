ae1a274fd73fcd3ef3491d74f56e43ddf10dc1d3
Gael Monachon <gaelmonach123@live.fr>
2020-09-18 10:39:22 +0200

    Clean master

--- AngularHybrid/app/app.ts
+++ AngularHybrid/app/app.ts
@@ -1,5 +1,4 @@
 ﻿import { downgradeComponent, downgradeInjectable } from "@angular/upgrade/static";
-import { HexService } from "../ng-app/hex.service";
 import { BaseService } from "./BaseService";
 import { ConvertController } from "./ConvertController";
 import { ConvertFormDirective } from "./ConvertFormDirective";
@@ -8,4 +7,4 @@ angular.module('app', [])
     .service('BaseService', BaseService)
     .controller('ConvertController', ConvertController)
     .directive('convertFormDirective', ConvertFormDirective)
-    .factory('HexService', downgradeInjectable(HexService) as any);
+    //.factory('HexService', downgradeInjectable(HexService) as any);
